05fb3c78eae73d2e04291755148235c285b19840
YIYU <rimiiii.u@gmail.com>
2023-10-26 07:54:08 +0000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "yiyu",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "machine learning engineer",
     bio: "I develop everything using node.",
     email: "rimiiii.u@gmail.com",
